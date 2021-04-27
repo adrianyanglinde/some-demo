2b97baf6012a82963f4cdbf876438a9c598e39e2
杨林德 <yanglinde@4399inc.com>
2021-04-27 11:14:03 +0800

    remove

--- app/components/page/TeTd/entry-teTd.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader/root';
-import ReactDom from 'react-dom';
-
-import Index from '@page/TeTd/Index.jsx';
-
-const AppProxy = hot(() => <Index />);
-ReactDom.render(<AppProxy />, document.getElementById('app'));
